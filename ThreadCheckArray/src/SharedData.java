abf350132c634f6f6b05bfeb517d83a7ee16a918
irisgrabois <irisgrabois@gmail.com>
2021-03-22 18:47:33 +0200

    iris

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,11 +1,34 @@
+import java.util.ArrayList;
+
+/**
+ * this is a class that contains all the shared data between the threads so they will be able to work on the same array and report to each other
+ * @author irbte
+ *
+ */
 public class SharedData 
 {
-	private int [] array;
+	/**
+	* a local  array of integers that we will check if there is a subgroup of numbers that their sum will be equal to b
+	 */
+	ArrayList<Integer> array = new ArrayList<Integer>();
+	/**
+	 * the winArray will have true at the indexes that are a part of the solution-their sum will be b
+	 */
 	private boolean [] winArray;
+	/**
+	 * the flag tells whether the current thread found a solution or not
+	 */
 	private boolean flag;
+	/**
+	 * b is the integer that we want to find a subgroup of numbers that their sum will be its value
+	 */
 	private final int b;
-	
-	public SharedData(int[] array, int b) {
+	/**
+	 * Constructor that recieves the array of the integers which will be the input for the problem,and the number b that will be checked for sum
+	 * @param array-array of integers as an input for the problem-looking for a subset ob sum b
+	 * @param b-the sum that will be looked for
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
@@ -21,7 +44,7 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	public ArrayList<Integer>  getArray() 
 	{
 		return array;
 	}
