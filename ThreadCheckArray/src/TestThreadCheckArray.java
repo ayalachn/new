da5b725de5182ff30ebeb5cf413be6c1ae304d7c
ayalachn <ayalachn@gmail.com>
2021-03-22 18:34:53 +0200

    change 1

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,22 +1,51 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * Gets input from user: array size, array and a number b
+ * Generates two threads that check the array
+ * when threads finished, main displays to user if there is a sub-array
+ * within the given array that the sum of it equals to number b
+ * @author Ayala
+ */
 public class TestThreadCheckArray {
+	/**
+	 * Get input from user: array length, array elements and number b.
+	 * Generates two threads to check the array
+	 * @param args - default parameters for main
+	 */
 	public static void main(String[] args) {
+		/**
+		 * get input from user
+		 */
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
+			/**
+			 * user enters array size
+			 */
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			ArrayList<Integer> array = new ArrayList<>();
 			System.out.println("Enter numbers for array");
-			
+			/**
+			 * user enters array elements
+			 */
 			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
-			
+				array.set(index, input.nextInt());
+			/**
+			 * user enters a number
+			 */
 			System.out.println("Enter number");
 			num = input.nextInt();
 			
+			/**
+			 * create new SharedData object with the
+			 * array and number
+			 */
 			SharedData sd = new SharedData(array, num);
 			
+			/**
+			 * create two threads & start them
+			 */
 			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
 			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
 			thread1.start();
@@ -30,6 +59,9 @@ public class TestThreadCheckArray {
 			{
 				e.printStackTrace();
 			}
+			/**
+			 * if there is no sub-array equal to b
+			 */
 			if (!sd.getFlag())
 			{
 				System.out.println("Sorry");
